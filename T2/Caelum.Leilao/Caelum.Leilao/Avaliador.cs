bc285b517d51c3bc6c11cfe0f3190312c4b93651
João Victor Vieira Paes <v013@benner.com.br>
2018-09-06 16:58:57 -0300

    Inicio do curso de TDD foi criado o projeto Caelum.Leilao

---
+++ T2/Caelum.Leilao/Caelum.Leilao/Avaliador.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caelum.Leilao
+{
+    public class Avaliador
+    {
+        private double maiorDeTodos = Double.MinValue;
+        private double menorDeTodos = Double.MaxValue;
+        private double mediaDeTodos = 0;    
+        public void Avalia(Leilao leilao)
+        {
+            double total = 0;
+            foreach(var lance in leilao.Lances)
+            {
+                if(lance.Valor > maiorDeTodos)
+                {
+                    maiorDeTodos = lance.Valor;
+                }
+                if(lance.Valor < menorDeTodos)
+                {
+                    menorDeTodos = lance.Valor;
+                }
+                total += lance.Valor;
+            }
+            mediaDeTodos = total / leilao.Lances.Count;
+        }
+
+        public double MaiorLance
+        {
+            get { return maiorDeTodos; }
+        }
+        public double MenorLance
+        {
+            get { return menorDeTodos; }
+        }
+        public double MediaDeTodos
+        {
+            get { return mediaDeTodos; }
+        }
+    }
+}
---
+++ T2/Caelum.Leilao/Caelum.Leilao/TesteDoAvaliador.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Caelum.Leilao
+{
+    [TestFixture]
+    public class AvaliadorTest
+    {
+        [Test]
+        public void DeveEntenderLancesEmOrdemCrescente()
+        {
+            // 1a parte: cenário
+            Usuario joao = new Usuario("João");
+            Usuario jose = new Usuario("Jose");
+            Usuario maria = new Usuario("Maria");
+
+            Leilao leilao = new Leilao("Playstation 3 Novo");
+
+            leilao.Propoe(new Lance(maria, 250.0));
+            leilao.Propoe(new Lance(joao, 300.0));
+            leilao.Propoe(new Lance(jose, 400));
+
+            // 2a parte: ação
+            Avaliador leiloeiro = new Avaliador();
+            leiloeiro.Avalia(leilao);
+
+            // 3a parte: validação
+            double maiorEsperado = 400;
+            double menorEsperado = 250;
+
+            Assert.AreEqual(maiorEsperado, leiloeiro.MaiorLance, 0.0001);
+            Assert.AreEqual(menorEsperado, leiloeiro.MenorLance, 0.0001);            
+        }
+
+        [Test]
+        public void DeveCalcularAMediaDeTodos()
+        {
+            Usuario joao = new Usuario("João");
+            Usuario jose = new Usuario("José");
+            Usuario maria = new Usuario("Maria");
+
+            Leilao leilao = new Leilao("Playstation 3 Novo");
+
+            leilao.Propoe(new Lance(maria, 300.0));
+            leilao.Propoe(new Lance(joao, 400.0));
+            leilao.Propoe(new Lance(jose, 500.0));
+
+            Avaliador leiloeiro = new Avaliador();
+            leiloeiro.Avalia(leilao);
+
+            Assert.AreEqual(400, leiloeiro.MediaDeTodos, 0.0001);
+        }
+    }
+}
