9a6b1ba7406591e4e4bd52b6a1b6d0900d50d89e
João Victor Vieira Paes <v013@benner.com.br>
2018-09-11 12:57:56 -0300

    TDD curso do Alura

--- T2/Caelum.Leilao/Caelum.Leilao/LanceTest.cs
+++ T2/Caelum.Leilao/Caelum.Leilao/LanceTest.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using NUnit.Framework;
+
 namespace Caelum.Leilao
 {
-    class LanceTest
+    [TestFixture]
+    public class LanceTest
     {
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DeveRecusarLancesComValorDeZero()
+        {
+            new Lance(new Usuario("John Doe"), 0);
+        }
+        [Test]
+        [Ex]
     }
 }
